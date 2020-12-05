97f86507de17e318e9c94e0a7eb9d425b4b3cc71
littlegao233 <gaoxinhong2004@hotmail.com>
2020-12-05 21:50:32 +0800

    v419

--- common/src/main/java/com/github/doctormacc/common/protocols/forwards/v408_to_v419_ForwardsPacketHandler.java
+++ common/src/main/java/com/github/doctormacc/common/protocols/forwards/v408_to_v419_ForwardsPacketHandler.java
@@ -3,13 +3,15 @@ package com.github.doctormacc.common.protocols.forwards;
 import com.github.doctormacc.common.BedrockBackwards;
 import com.github.doctormacc.common.PlayerSession;
 import com.nukkitx.protocol.bedrock.BedrockPacket;
+import com.nukkitx.protocol.bedrock.BedrockPacketType;
 import com.nukkitx.protocol.bedrock.packet.StartGamePacket;
 
 public class v408_to_v419_ForwardsPacketHandler extends ForwardsPacketHandler {
 
     @Override
     public boolean translate(PlayerSession session, BedrockPacket packet, boolean fromUpstream, int translatorIndex) {
-        if ( packet.getPacketId()==156) {
+
+        if ( packet.getPacketId()==156||packet.getPacketType()== BedrockPacketType.UPDATE_BLOCK_PROPERTIES) {
             BedrockBackwards.LOGGER.debug("Ignoring packet " + packet.getPacketType());
             return false;
         }
